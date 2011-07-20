9e127594e4b50404dddc97c85225dd91600400d0
Samir Talwar <samir@noodlesandwich.com>
2011-07-21 00:05:13 +0100

    Reworked the success/fail handling.

--- smoke.rb
+++ smoke.rb
@@ -3,47 +3,63 @@
 TEST_CASE = ARGV[0]
 APPLICATION = ARGV[1]
 
+GREEN = "\033[31m"
+RED = "\033[32m"
+RESET = "\033[0m"
+
 tests = Dir.glob("#{TEST_CASE}/*.in").collect do |input_file|
   name = input_file[/(?<=#{TEST_CASE}\/).*(?=\.in)/]
   input = IO.read(input_file).strip
 
   output_files = Dir.glob "#{TEST_CASE}/#{name}.out*"
   potential_outputs = output_files.collect do |output_file|
-    IO.read(output_file).strip
+    IO.read(output_file).strip.inspect
   end
 
   [name, input, potential_outputs]
 end
 
-def succeeded message
-  puts "\033[32m#{message}\033[0m"
+def green string
+  "#{GREEN}#{string}#{RESET}"
+end
+
+def red string
+  "#{RED}#{string}#{RESET}"
+end
+
+def succeeded *messages
+  puts green messages.collect { |message| '  ' + message }.join "\n"
 end
 
 @failures = 0
-def failed message
-  puts "\033[31m#{message}\033[0m"
+def failed *messages
+  puts red messages.collect { |message| '  ' + message }.join "\n"
   @failures += 1
 end
 
 tests.each do |name, input, potential_outputs|
-  failed "#{name}: no outputs provided" if potential_outputs.length == 0
+  puts name
+  failed "no outputs provided" if potential_outputs.length == 0
 
   output = IO.popen APPLICATION, 'r+' do |io|
     io.write input
-    io.read.strip
+    io.read.strip.inspect
   end
 
   unless potential_outputs.include? output
-    next failed "#{name}:\n  output: #{output.inspect}\n  expected: #{potential_outputs[0].inspect}" if potential_outputs.length == 1
-    next failed "#{name}:\n  output: #{output.inspect}\n  expected: #{potential_outputs[0...potential_outputs.length - 1].join(', ')} or #{potential_outputs[-1]}"
+    if potential_outputs.length == 1
+      next failed "output: #{output}", "expected: #{potential_outputs[0]}" 
+    end
+
+    next failed "output: #{output}", "expected: #{potential_outputs[0...potential_outputs.length - 1].join(', ')} or #{potential_outputs[-1]}"
   end
 
-  succeeded name
+  succeeded 'succeeded'
 end
 
 puts
 if @failures > 0
-  failed "#{tests.length} tests, #{@failures} failures"
+  puts red "#{tests.length} tests, #{@failures} failures"
 else
-  succeeded "#{tests.length} tests, #{@failures} failures"
+  puts green "#{tests.length} tests, #{@failures} failures"
 end

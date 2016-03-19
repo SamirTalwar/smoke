bf27b3f59a7eed8f9999f3d72c43601f6e743020
Samir Talwar <samir@noodlesandwich.com>
2016-03-19 00:52:59 +0000

    Monkey-patch for Ruby 1.9.3 support.

--- fixtures/pick.rb
+++ fixtures/pick.rb
@@ -1,7 +1,4 @@
 #!/usr/bin/env ruby
 
-if ARGV.empty?
-  puts $stdin.read.lines.sample
-else
-  puts ARGV.sample
-end
+values = ARGV.empty? ? $stdin.read.lines.collect(&:strip) : ARGV
+puts values[rand(values.length)]

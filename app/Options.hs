9b8f878c6f9cbe90ff775dffc291a432c810fa27
Samir Talwar <samir@noodlesandwich.com>
2017-10-08 01:07:25 +0100

    Turn on all warnings, and fix them.

--- app/Options.hs
+++ app/Options.hs
@@ -1,9 +1,8 @@
 module Options
-  ( Options
+  ( Options(..)
   , parseOptions
   ) where
 
-import Data.Maybe (fromMaybe)
 import Data.Semigroup ((<>))
 import Options.Applicative
 import System.Posix.IO (stdOutput)
@@ -17,27 +16,33 @@ data Options = Options
   , testLocations :: [FilePath]
   } deriving (Eq, Show)
 
+parseOptions :: IO Options
 parseOptions = do
   isTTY <- queryTerminal stdOutput
   execParser (options isTTY)
 
+options :: Bool -> ParserInfo Options
 options isTTY =
   info
     (optionParser isTTY <**> helper)
     (fullDesc <>
      header "Smoke: a framework for testing most things from the very edges.")
 
+optionParser :: Bool -> Parser Options
 optionParser isTTY =
   Options <$> commandParser <*> colorParser isTTY <*> testLocationParser
 
+commandParser :: Parser (Maybe Command)
 commandParser =
   optional
     (words <$>
      strOption (long "command" <> help "Specify or override the command to run"))
 
+colorParser :: Bool -> Parser Bool
 colorParser isTTY =
   flag' True (short 'c' <> long "color" <> help "Color output") <|>
   flag' False (long "no-color" <> help "Do not color output") <|>
   pure isTTY -- TODO: Make this work on Windows.
 
+testLocationParser :: Parser [FilePath]
 testLocationParser = many (argument str (metavar "TEST-LOCATION..."))

d67c499a47a5708912d2aa8dd201b1b425f3e8d3
Samir Talwar <samir@noodlesandwich.com>
2017-10-08 02:05:09 +0100

    Extract functions to reduce nesting.

--- src/Test/Smoke.hs
+++ src/Test/Smoke.hs
@@ -15,6 +15,7 @@ import Data.Maybe (fromJust, fromMaybe, isNothing)
 import System.Directory
 import System.FilePath
 import System.FilePath.Glob
+import Test.Smoke.FileTypes (FileType)
 import qualified Test.Smoke.FileTypes as FileTypes
 
 type Command = [String]
@@ -44,52 +45,60 @@ discoverTests options =
   concat <$>
   forM
     (optionsTestLocations options)
-    (\location -> do
-       commandForLocation <-
-         return (optionsCommand options) <<|>>
-         readCommandFileIfExists (location </> "command") <<|>>
-         readCommandFileIfExists (takeDirectory location </> "command")
-       files <-
-         sortBy (compare `on` snd) .
-         concat .
-         zipWith
-           (\fileTypeGlob paths -> zip (repeat fileTypeGlob) paths)
-           (map fst FileTypes.globs) .
-         fst <$>
-         globDir (map snd FileTypes.globs) location
-       let grouped = groupBy ((==) `on` (dropExtension . snd)) files
-       forM grouped $ \group -> do
-         let part fileType = snd <$> find ((== fileType) . fst) group
-         let parts fileType = snd <$> filter ((== fileType) . fst) group
-         let name = dropExtension (snd (head group))
-         command <-
-           sequence (readCommandFile <$> part FileTypes.Command) <<|>>
-           return commandForLocation
-         args <- sequence (readCommandFile <$> part FileTypes.Args)
-         let stdIn = part FileTypes.StdIn
-         let stdOut = parts FileTypes.StdOut
-         let stdErr = parts FileTypes.StdErr
-         status <-
-           fromMaybe (return 0) (readStatusFile <$> part FileTypes.Status)
-         when
-           (isNothing command)
-           (fail ("The test \"" ++ name ++ "\" has no command."))
-         when
-           (isNothing args && isNothing stdIn)
-           (fail ("The test \"" ++ name ++ "\" has no args or STDIN."))
-         when
-           (null stdOut && null stdErr)
-           (fail ("The test \"" ++ name ++ "\" has no STDOUT or STDERR."))
-         return
-           Test
-           { testName = name
-           , testCommand = fromJust command
-           , testArgs = args
-           , testStdIn = stdIn
-           , testStdOut = stdOut
-           , testStdErr = stdErr
-           , testStatus = status
-           })
+    (discoverTestsInLocation (optionsCommand options))
+
+discoverTestsInLocation :: Maybe Command -> FilePath -> IO [Test]
+discoverTestsInLocation commandFromOptions location = do
+  command <- findCommand
+  files <- allFiles
+  let grouped = groupBy ((==) `on` (dropExtension . snd)) files
+  forM grouped (constructTestFromGroup command)
+  where
+    findCommand =
+      return commandFromOptions <<|>>
+      readCommandFileIfExists (location </> "command") <<|>>
+      readCommandFileIfExists (takeDirectory location </> "command")
+    allFiles =
+      sortBy (compare `on` snd) .
+      concat .
+      zipWith
+        (\fileTypeGlob paths -> zip (repeat fileTypeGlob) paths)
+        (map fst FileTypes.globs) .
+      fst <$>
+      globDir (map snd FileTypes.globs) location
+
+constructTestFromGroup :: Maybe Command -> [(FileType, FilePath)] -> IO Test
+constructTestFromGroup commandForLocation group = do
+  let part fileType = snd <$> find ((== fileType) . fst) group
+  let parts fileType = snd <$> filter ((== fileType) . fst) group
+  let name = dropExtension (snd (head group))
+  command <-
+    sequence (readCommandFile <$> part FileTypes.Command) <<|>>
+    return commandForLocation
+  args <- sequence (readCommandFile <$> part FileTypes.Args)
+  let stdIn = part FileTypes.StdIn
+  let stdOut = parts FileTypes.StdOut
+  let stdErr = parts FileTypes.StdErr
+  status <- fromMaybe (return 0) (readStatusFile <$> part FileTypes.Status)
+  when
+    (isNothing command)
+    (fail ("The test \"" ++ name ++ "\" has no command."))
+  when
+    (isNothing args && isNothing stdIn)
+    (fail ("The test \"" ++ name ++ "\" has no args or STDIN."))
+  when
+    (null stdOut && null stdErr)
+    (fail ("The test \"" ++ name ++ "\" has no STDOUT or STDERR."))
+  return
+    Test
+    { testName = name
+    , testCommand = fromJust command
+    , testArgs = args
+    , testStdIn = stdIn
+    , testStdOut = stdOut
+    , testStdErr = stdErr
+    , testStatus = status
+    }
 
 readCommandFileIfExists :: FilePath -> IO (Maybe Command)
 readCommandFileIfExists path = do

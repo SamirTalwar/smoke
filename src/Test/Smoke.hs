a58bb74a7eb292a8de10582fafe4107d141d20d2
Samir Talwar <samir@noodlesandwich.com>
2017-10-08 01:57:28 +0100

    Construct the test cases.

--- src/Test/Smoke.hs
+++ src/Test/Smoke.hs
@@ -7,14 +7,19 @@ module Test.Smoke
   , runTests
   ) where
 
-import Control.Applicative
-import Control.Monad
+import Control.Applicative ((<|>))
+import Control.Monad (forM, liftM2, when)
+import Data.Function (on)
+import Data.List (find, groupBy, sortBy)
+import Data.Maybe (fromJust, fromMaybe, isNothing)
 import System.Directory
 import System.FilePath
 import System.FilePath.Glob
 import qualified Test.Smoke.FileTypes as FileTypes
 
 type Command = [String]
+
+type Args = [String]
 
 data Options = Options
   { optionsCommand :: Maybe Command
@@ -25,7 +30,13 @@ data Options = Options
 type Tests = [Test]
 
 data Test = Test
-  {
+  { testName :: String
+  , testCommand :: Command
+  , testArgs :: Maybe Args
+  , testStdIn :: Maybe FilePath
+  , testStdOut :: [FilePath]
+  , testStdErr :: [FilePath]
+  , testStatus :: Int
   } deriving (Eq, Show)
 
 discoverTests :: Options -> IO Tests
@@ -34,27 +45,64 @@ discoverTests options =
   forM
     (optionsTestLocations options)
     (\location -> do
-       command <-
+       commandForLocation <-
          return (optionsCommand options) <<|>>
-         readCommandFile (location </> "command") <<|>>
-         readCommandFile (takeDirectory location </> "command")
+         readCommandFileIfExists (location </> "command") <<|>>
+         readCommandFileIfExists (takeDirectory location </> "command")
        files <-
+         sortBy (compare `on` snd) .
          concat .
          zipWith
            (\fileTypeGlob paths -> zip (repeat fileTypeGlob) paths)
            (map fst FileTypes.globs) .
          fst <$>
          globDir (map snd FileTypes.globs) location
-       print command
-       print files
-       return [])
+       let grouped = groupBy ((==) `on` (dropExtension . snd)) files
+       forM grouped $ \group -> do
+         let part fileType = snd <$> find ((== fileType) . fst) group
+         let parts fileType = snd <$> filter ((== fileType) . fst) group
+         let name = dropExtension (snd (head group))
+         command <-
+           sequence (readCommandFile <$> part FileTypes.Command) <<|>>
+           return commandForLocation
+         args <- sequence (readCommandFile <$> part FileTypes.Args)
+         let stdIn = part FileTypes.StdIn
+         let stdOut = parts FileTypes.StdOut
+         let stdErr = parts FileTypes.StdErr
+         status <-
+           fromMaybe (return 0) (readStatusFile <$> part FileTypes.Status)
+         when
+           (isNothing command)
+           (fail ("The test \"" ++ name ++ "\" has no command."))
+         when
+           (isNothing args && isNothing stdIn)
+           (fail ("The test \"" ++ name ++ "\" has no args or STDIN."))
+         when
+           (null stdOut && null stdErr)
+           (fail ("The test \"" ++ name ++ "\" has no STDOUT or STDERR."))
+         return
+           Test
+           { testName = name
+           , testCommand = fromJust command
+           , testArgs = args
+           , testStdIn = stdIn
+           , testStdOut = stdOut
+           , testStdErr = stdErr
+           , testStatus = status
+           })
 
-readCommandFile :: FilePath -> IO (Maybe Command)
-readCommandFile path = do
+readCommandFileIfExists :: FilePath -> IO (Maybe Command)
+readCommandFileIfExists path = do
   exists <- doesFileExist path
   if exists
-    then Just . lines <$> readFile path
+    then Just <$> readCommandFile path
     else return Nothing
+
+readCommandFile :: FilePath -> IO Command
+readCommandFile path = lines <$> readFile path
+
+readStatusFile :: FilePath -> IO Int
+readStatusFile path = read <$> readFile path
 
 runTests :: Tests -> IO ()
 runTests tests = do

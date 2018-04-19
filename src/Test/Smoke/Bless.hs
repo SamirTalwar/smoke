82cbaba50ddf2c21b890e85596b4b75889df2ce3
Samir Talwar <samir@noodlesandwich.com>
2018-04-19 20:13:04 +0100

    Bless: Create and delete files when appropriate.

--- src/Test/Smoke/Bless.hs
+++ src/Test/Smoke/Bless.hs
@@ -2,7 +2,11 @@ module Test.Smoke.Bless
   ( blessResults
   ) where
 
+import Control.Monad (unless)
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString
+import System.Directory (doesFileExist, removeFile)
+import Test.Smoke.FileTypes (filePath)
 import qualified Test.Smoke.FileTypes as FileTypes
 import Test.Smoke.Types
 
@@ -15,19 +19,33 @@ blessResult (TestFailure TestExecutionPlan {planTest = test} _ (PartFailure (_:_
 blessResult (TestFailure TestExecutionPlan {planTest = test} _ _ (PartFailure (_:_:_) _)) =
   return $ TestError test CouldNotBlessStdErrWithMultipleValues
 blessResult (TestFailure (TestExecutionPlan test@(Test name location _ _ _ stdOutPaths stdErrPaths _) _ _ _) status stdOut stdErr) = do
+  let statusPath = filePath location name FileTypes.Status
+  let stdOutPath =
+        case stdOutPaths of
+          (x:_) -> x
+          [] -> filePath location name FileTypes.StdOut
+  let stdErrPath =
+        case stdErrPaths of
+          (x:_) -> x
+          [] -> filePath location name FileTypes.StdErr
   case status of
-    PartFailure _ (Status actual) ->
-      writeFile
-        (FileTypes.filePath location name FileTypes.Status)
-        (show actual ++ "\n")
+    PartFailure _ (Status 0) -> removeFile statusPath
+    PartFailure _ (Status actual) -> writeFile statusPath (show actual ++ "\n")
     _ -> return ()
   case stdOut of
-    PartFailure _ (StdOut actual) ->
-      ByteString.writeFile (head stdOutPaths) actual
+    PartFailure _ (StdOut actual) -> writeOutput stdOutPath actual
     _ -> return ()
   case stdErr of
-    PartFailure _ (StdErr actual) ->
-      ByteString.writeFile (head stdErrPaths) actual
+    PartFailure _ (StdErr actual) -> writeOutput stdErrPath actual
     _ -> return ()
+  stdOutExists <- doesFileExist stdOutPath
+  stdErrExists <- doesFileExist stdErrPath
+  unless (stdOutExists || stdErrExists) $
+    writeOutput stdOutPath ByteString.empty
   return (TestSuccess test)
 blessResult result = return result
+
+writeOutput :: FilePath -> ByteString -> IO ()
+writeOutput path contents
+  | contents == ByteString.empty = removeFile path
+  | otherwise = ByteString.writeFile path contents

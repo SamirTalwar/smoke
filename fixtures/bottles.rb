01aeb2d76c0d9ffddfcab4bc488a6fd05b76f11d
Samir Talwar <samir@noodlesandwich.com>
2018-07-06 10:13:35 +0100

    Test long, broken output.

--- fixtures/bottles.rb
+++ fixtures/bottles.rb
@@ -11,11 +11,26 @@ def bottles(n)
   end
 end
 
-start = ARGV[0].to_i
-start.downto(1).each do |n|
+def broken(n)
+  if n % 111 == 0
+    n / 2
+  else
+    n
+  end
+end
+
+if ARGV[0] == '--broken'
+  start = ARGV[1].to_i
+  counter = start.downto(1).map { |n| broken(n) }
+else
+  start = ARGV[0].to_i
+  counter = start.downto(1)
+end
+
+counter.each do |n|
   puts "#{bottles(n).capitalize} of beer on the wall, #{bottles n} of beer."
   puts "Take one down and pass it around, #{bottles(n - 1)} of beer on the wall."
 end
 
 puts 'No more bottles of beer on the wall, no more bottles of beer.'
-puts "Go to the store and buy some more, #{bottles start} of beer on the wall."
+puts "Go to the store and buy some more, #{bottles counter.first} of beer on the wall."

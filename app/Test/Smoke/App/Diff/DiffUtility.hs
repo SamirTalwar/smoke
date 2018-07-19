cde2d06491bf2208bf0f5c2c5b445c7244eeaa5b
Samir Talwar <samir@noodlesandwich.com>
2018-07-19 11:27:25 +0100

    Diff: Extract out the code to run `diff` to use it for `git diff`.

--- app/Test/Smoke/App/Diff/DiffUtility.hs
+++ app/Test/Smoke/App/Diff/DiffUtility.hs
@@ -1,47 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Test.Smoke.App.Diff.DiffUtility
   ( engine
   ) where
 
-import Control.Exception (throwIO)
-import qualified Data.ByteString as ByteString
-import Data.ByteString.Char8 (unpack)
-import Data.Maybe (isJust)
-import System.Directory (findExecutable)
-import System.Exit (ExitCode(..))
-import System.IO (hClose)
-import System.IO.Temp (withSystemTempFile)
-import System.Process.ByteString (readProcessWithExitCode)
+import qualified Data.List.NonEmpty as NonEmpty
+import Test.Smoke.App.Diff.ExternalDiffCommand
 import Test.Smoke.App.Diff.Types
 
 engine :: DiffEngine
 engine =
-  DiffEngine {engineName = name, engineEnabled = enabled, engineRender = render}
+  DiffEngine
+    { engineName = name
+    , engineEnabled = enabled command
+    , engineRender = render command
+    }
 
 name :: String
 name = "diff"
 
-executable :: String
-executable = "diff"
-
-enabled :: IO Bool
-enabled = isJust <$> findExecutable executable
-
-render :: RenderDiff
-render left right =
-  withSystemTempFile "smoke-left-" $ \leftFilePath leftFile ->
-    withSystemTempFile "smoke-right-" $ \rightFilePath rightFile -> do
-      ByteString.hPut leftFile left
-      ByteString.hPut rightFile right
-      hClose leftFile
-      hClose rightFile
-      (exitCode, stdout, stderr) <-
-        readProcessWithExitCode executable [leftFilePath, rightFilePath] ""
-      case exitCode of
-        ExitSuccess -> return stdout
-        ExitFailure 1 -> return stdout
-        ExitFailure code ->
-          throwIO $
-          userError $
-          "`diff` failed with status " ++ show code ++ ".\n" ++ unpack stderr
+command :: Command
+command = NonEmpty.fromList ["diff"]
---
+++ app/Test/Smoke/App/Diff/ExternalDiffCommand.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Smoke.App.Diff.ExternalDiffCommand
+  ( Command
+  , enabled
+  , render
+  ) where
+
+import Control.Exception (throwIO)
+import qualified Data.ByteString as ByteString
+import Data.ByteString.Char8 (unpack)
+import qualified Data.List.NonEmpty as NonEmpty
+import Data.List.NonEmpty (NonEmpty((:|)))
+import Data.Maybe (isJust)
+import System.Directory (findExecutable)
+import System.Exit (ExitCode(..))
+import System.IO (hClose)
+import System.IO.Temp (withSystemTempFile)
+import System.Process.ByteString (readProcessWithExitCode)
+import Test.Smoke.App.Diff.Types
+
+type Command = NonEmpty String
+
+enabled :: Command -> IO Bool
+enabled (executable :| _) = isJust <$> findExecutable executable
+
+render :: Command -> RenderDiff
+render (command@(executable :| args)) left right =
+  withSystemTempFile "smoke-left-" $ \leftFilePath leftFile ->
+    withSystemTempFile "smoke-right-" $ \rightFilePath rightFile -> do
+      ByteString.hPut leftFile left
+      ByteString.hPut rightFile right
+      hClose leftFile
+      hClose rightFile
+      (exitCode, stdout, stderr) <-
+        readProcessWithExitCode
+          executable
+          (args ++ [leftFilePath, rightFilePath])
+          ""
+      case exitCode of
+        ExitSuccess -> return stdout
+        ExitFailure 1 -> return stdout
+        ExitFailure code ->
+          throwIO $
+          userError $
+          "`" ++
+          unwords (NonEmpty.toList command) ++
+          "`" ++
+          " failed with status " ++ show code ++ "." ++ "\n" ++ unpack stderr

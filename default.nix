d0dc9480195130bfdbc1e8b687d492ecc1f0b155
Samir Talwar <samir@noodlesandwich.com>
2019-09-24 10:57:17 +0100

    Release v2.1.0.

--- default.nix
+++ default.nix
@@ -12,7 +12,7 @@ let
       }:
       mkDerivation {
         pname = "smoke";
-        version = "2.0.0";
+        version = "2.1.0";
         src = ./.;
         isLibrary = true;
         isExecutable = true;

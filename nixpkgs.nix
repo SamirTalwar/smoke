b9a34ffa55acc265cc9b2f488e4dd7ea83c1e7fa
Samir Talwar <samir@noodlesandwich.com>
2020-04-04 14:33:58 +0200

    Update nixpkgs to the latest published version.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -6,8 +6,8 @@ let
 
   nixpkgs-unstable = import (
     builtins.fetchTarball {
-      url = https://github.com/NixOS/nixpkgs/archive/6a3ce692c0bdc393e2ae3a98b30778b86e0708b0.tar.gz;
-      sha256 = "1cf88dadrk8m3447skl6zrldn2r59j4dzygsbi94myvq128h83mw";
+      url = https://github.com/NixOS/nixpkgs-channels/archive/05f0934825c2a0750d4888c4735f9420c906b388.tar.gz;
+      sha256 = "1g8c2w0661qn89ajp44znmwfmghbbiygvdzq0rzlvlpdiz28v6gy";
     }
   );
 in

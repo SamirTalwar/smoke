9bd1b407f096dc680dd0ca5eee4b500a6a0afc8b
Samir Talwar <samir@noodlesandwich.com>
2020-04-13 16:09:22 +0200

    Pull the nixpkgs version out into a file.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,13 +1,14 @@
-{}:
+{ lib ? (import <nixpkgs> {}).lib }:
 let
   config = {};
 
   overlays = [];
 
+  version = lib.strings.fileContents ./nixpkgs.version;
+
   nixpkgs-unstable = import (
     builtins.fetchTarball {
-      url = https://github.com/NixOS/nixpkgs-channels/archive/05f0934825c2a0750d4888c4735f9420c906b388.tar.gz;
-      sha256 = "1g8c2w0661qn89ajp44znmwfmghbbiygvdzq0rzlvlpdiz28v6gy";
+      url = https://github.com/NixOS/nixpkgs-channels/archive/ + version + ".tar.gz";
     }
   );
 in

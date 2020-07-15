f8bc7636f1aeffb4076f18f7929d1fdf5cd99d4f
Samir Talwar <samir@noodlesandwich.com>
2020-07-15 09:52:06 +0200

    Rewrite Maps.mapWithKeyM using `assocs` and `do`.
    Makes later versions of HLint happy.

--- src/lib/Test/Smoke/Maps.hs
+++ src/lib/Test/Smoke/Maps.hs
@@ -7,11 +7,11 @@ mapFromTraversable :: (Foldable f, Ord a) => f (a, b) -> Map a b
 mapFromTraversable = foldMap (uncurry singleton)
 
 mapWithKeyM :: (Monad m, Ord k) => (k -> v1 -> m v2) -> Map k v1 -> m (Map k v2)
-mapWithKeyM f oldMap =
-  foldM
-    (\newMap key -> insert <$> pure key <*> f key (oldMap ! key) <*> pure newMap)
-    empty
-    (keys oldMap)
+mapWithKeyM f oldMap = foldM transform empty (assocs oldMap)
+  where
+    transform newMap (key, oldValue) = do
+      newValue <- f key oldValue
+      return $ insert key newValue newMap
 
 forWithKeyM :: (Monad m, Ord k) => Map k v1 -> (k -> v1 -> m v2) -> m (Map k v2)
 forWithKeyM = flip mapWithKeyM

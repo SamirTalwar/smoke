b82373a31ad3e4f4a2365ef16fa9d3a79ec5ddba
Samir Talwar <samir@noodlesandwich.com>
2022-08-07 19:52:45 +0200

    Patch `tar` in nixpkgs accordingly.

--- nix/ghc.nix
+++ nix/ghc.nix
@@ -1,8 +1,11 @@
 { lib
 , haskell
 , ghcVersion ? lib.strings.fileContents ../ghc.version
+, overrides ? _: _: { }
 }:
 let
   compiler = "ghc" + lib.strings.stringAsChars (c: if c == "." then "" else c) ghcVersion;
 in
-haskell.packages."${compiler}"
+haskell.packages."${compiler}".override {
+  inherit overrides;
+}

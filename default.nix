793f3cd3115021785b8a57e4e81042361fb9930c
Samir Talwar <samir@functional.computer>
2026-04-14 14:35:47 +0200

    Build Smoke with vanilla Nix, without flakes.
    This rewrites `default.nix` and `shell.nix` to be standalone, and calls
    into them from `flake.nix`, rather than the other way around.
    
    To do this, we introduce npins as the canonical pinning service, using
    it to pin nixpkgs. The flake no longer pins nixpkgs, only flake-utils.

--- default.nix
+++ default.nix
@@ -1,10 +1,27 @@
-(import
-  (
-    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-  )
-  { src = ./.; }
-).defaultNix
+{ system ? builtins.currentSystem
+, sources ? import ./nix/npins
+, pkgs ? import sources.nixpkgs { inherit system; }
+, ghcVersion ? pkgs.lib.strings.fileContents ./ghc.version
+, haskell ? import ./nix/haskell.nix { inherit system sources pkgs ghcVersion; }
+}:
+let
+  # required libraries with static linking enabled
+  staticLibs = with pkgs; [
+    (gmp6.override { withStatic = true; })
+    (libffi.overrideAttrs (old: {
+      dontDisableStatic = true;
+    }))
+    (libiconv.overrideAttrs (old: {
+      enableStatic = true;
+      enableShared = false;
+    }))
+    (openssl.override { static = true; })
+    zlib.static
+  ];
+
+  drv = haskell.packages.callCabal2nix "smoke" (pkgs.nix-gitignore.gitignoreSource [ ] ./.) {
+    # Override tar with the patched version; see stack.yaml for details.
+    tar = haskell.packages.tar_0_6_3_0;
+  };
+in
+haskell.lib.addExtraLibraries drv staticLibs
